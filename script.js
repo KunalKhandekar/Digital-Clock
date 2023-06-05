675e0297acaf2f4f9120fecab3d802c30e812371
KunalKhandekar <kunalkhandekar010@gmail.com>
2023-06-05 08:54:55 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+setInterval(() => {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    
+    if (h > 12) {
+        h = h - 12;
+        day_night.textContent = "PM" 
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    let hour = document.querySelector(".hours").firstElementChild
+    hour.textContent = h;
+    let miunte = document.querySelector(".minutes").firstElementChild
+    miunte.textContent = m;
+    let second = document.querySelector(".seconds").firstElementChild
+    second.textContent = s;
+    let day_night = document.querySelector(".day_night").firstElementChild
+})
